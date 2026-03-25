b21ffe38ce16f70533c886329e1d2c0067832649
Javi <javi@gmail.com>
2026-03-25 20:53:11 +0100

    fusionando rama personal con master

--- src/practica/main.java
+++ src/practica/main.java
@@ -2,11 +2,12 @@ package practica;
 
 public class main {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Listado de alumnos Ciudad Escolar");
-		System.out.println("Alumno 1:");
-
-	}
+	 public static void main(String[] args) {
+	        System.out.println("Listado de alumnos Ciudad Escolar");
+	        System.out.println("Alumno 1:");
+	        
+	        Alumno alumno1 = new Alumno("Juan", "García", "12345678A");
+	        System.out.println(alumno1);
+	    }
 
 }
