5df793d7da3892cb8a42be4ef0ab800b73e668a1
Javi <javi@gmail.com>
2026-03-25 17:25:08 +0100

    añadiendo constructor con parámetros

--- src/practica/Alumno.java
+++ src/practica/Alumno.java
@@ -1,7 +1,14 @@
 package practica;
 
 public class Alumno {
+	
     private String nombre;
     private String apellido;
     private String dni;
+    
+    public Alumno(String nombre, String apellido, String dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
 }
